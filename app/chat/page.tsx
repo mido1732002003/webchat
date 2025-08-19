a57150480939a8237ded0f213c1ca0d9820d75c1
mido1732002003 <mido.abdallah1732003@gmail.com>
2025-08-19 04:12:39 +0300

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import { supabase } from '@/lib/supabaseClient'
 import { User } from '@supabase/supabase-js'
 import { Message, Profile } from '@/types'
